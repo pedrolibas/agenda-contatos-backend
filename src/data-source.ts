ca21d8be34400f57701938f58c8c4c6db94eed4b
Pedro Gabriel <pedrogv03@hotmail.com>
2023-02-02 00:37:30 -0300

    feat: create server and database

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,6 +4,16 @@ import "dotenv/config"
 const AppDataSource = new DataSource(
     {
         type: "postgres",
-        host: process.env.HOST
+        host: process.env.HOST,
+        port: 5432,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        logging: true,
+        synchronize: false,
+        entities: ["src/entities/*.ts"],
+        migrations: ["src/migrations/*.ts"]
     }
 )
+
+export default AppDataSource
